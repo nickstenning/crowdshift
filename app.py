e8deea9d3e9c8e719e9a80730cc5043733a2d3b2
Nick Stenning <nick@whiteink.com>
2012-06-09 14:14:52 -0400

    Correct default bind

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import crowdshift
 if __name__ == '__main__':
     app = crowdshift.create_app()
 
-    host = environ.get('HOST', '127.0.0.1')
+    host = environ.get('HOST', '0.0.0.0')
     port = int(environ.get('PORT', 5000))
     debug = environ.get('DEBUG') is not None
 
